5923f2c7fc757fc732fda0f5ba224dfc7764531c
mkanuch <mkanuch@gmail.com>
2025-01-31 16:50:37 +0100

    removing files

--- leaderboard.js
+++
@@ -1,7 +0,0 @@
-export function displayLeaderboard(users) {
-    users.sort((a, b) => b.points - a.points);
-    console.log("🏆 Leaderboard:");
-    users.forEach((user, index) => {
-        console.log(`${index + 1}. ${user.nickname} - ${user.points} bodů (Úroveň: ${user.level})`);
-    });
-}
